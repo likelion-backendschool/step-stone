298814455bf30f22ec6ca20c392271cd15c807fc
YeonJun Cho <micayel1219@gmail.com>
2022-08-05 17:56:37 +0900

    Feat : 브랜치 생성 커밋

--- StepStone/src/main/java/comlikelion/stepstone/StepstoneApplication.java
+++ StepStone/src/main/java/comlikelion/stepstone/StepstoneApplication.java
@@ -3,6 +3,10 @@ package comlikelion.stepstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * ChoYeonJun 브랜치 생성 ^_^
+ */
 @SpringBootApplication
 public class StepstoneApplication {
 
