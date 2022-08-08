4968e5afee49c310ba358cbfd77fe47cdee1450d
YeonJun Cho <micayel1219@gmail.com>
2022-08-08 16:23:44 +0900

    Feat : PostController Annotaion 생성

--- StepStone/src/main/java/comlikelion/stepstone/post/PostController.java
+++ StepStone/src/main/java/comlikelion/stepstone/post/PostController.java
@@ -1,4 +1,12 @@
 package comlikelion.stepstone.post;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/post")
 public class PostController {
+
 }
