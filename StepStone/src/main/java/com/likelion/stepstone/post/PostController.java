a0a05639c06df0e4407952944e46b3330c889212
jwp345 <jwp135@naver.com>
2022-08-09 19:59:20 +0900

    Feat : 페이징 구현

--- StepStone/src/main/java/com/likelion/stepstone/post/PostController.java
+++ StepStone/src/main/java/com/likelion/stepstone/post/PostController.java
@@ -1,12 +1,16 @@
 package com.likelion.stepstone.post;
 
 import com.likelion.stepstone.post.model.PostDto;
+import com.likelion.stepstone.post.model.PostVo;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -37,4 +41,21 @@ public class PostController {
         return "post/create";
     }
 
+    @GetMapping("/read/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, Model model) {
+
+        Page<PostVo> page = postService.findPaginated(1, 3, "likes", "desc");
+        List<PostVo> likePosts = page.getContent();
+
+        model.addAttribute("likePosts", likePosts);
+
+        Page<PostVo> currPage = postService.findPaginated(pageNo, 10, "createdAt", "desc");
+        List<PostVo> listPosts = currPage.getContent();
+
+        model.addAttribute("listPosts", listPosts);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", currPage.getTotalPages());
+        model.addAttribute("totalItems", currPage.getTotalElements());
+        return "index";
+    }
 }
--- StepStone/src/main/java/com/likelion/stepstone/post/PostRepository.java
+++ StepStone/src/main/java/com/likelion/stepstone/post/PostRepository.java
@@ -1,7 +1,10 @@
 package com.likelion.stepstone.post;
 
 import com.likelion.stepstone.post.model.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+  Page<PostEntity> findAll(Pageable pageable);
 }
