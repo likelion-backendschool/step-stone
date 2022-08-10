ab951014046437c4e73a48cc26c81de370d7b2e3
nellizi <masterliz9095@gmail.com>
2022-08-11 02:00:19 +0900

    LikeController 완성

--- StepStone/src/main/java/com/likelion/stepstone/like/LikeController.java
+++ StepStone/src/main/java/com/likelion/stepstone/like/LikeController.java
@@ -1,9 +1,31 @@
 package com.likelion.stepstone.like;
 
+import com.likelion.stepstone.like.model.LikeDto;
+import com.likelion.stepstone.like.model.LikeEntity;
+import com.likelion.stepstone.post.model.PostDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.UUID;
 
 @Controller
 public class LikeController {
+    @Autowired
+    private  LikeService likeService;
+    @PostMapping("/post/{postId}/likes")
+    public void likes(@PathVariable UUID postId){
+        UUID userId = UUID.randomUUID();
+
+        LikeDto likeDto = LikeDto.builder()
+                .postId(postId)
+                .userId(userId)
+                .build();
+
+        likeService.likes(likeDto);
+    }
+
 
 
 }
--- StepStone/src/main/java/com/likelion/stepstone/like/LikeService.java
+++ StepStone/src/main/java/com/likelion/stepstone/like/LikeService.java
@@ -1,7 +1,23 @@
 package com.likelion.stepstone.like;
 
+import com.likelion.stepstone.like.model.LikeDto;
+import com.likelion.stepstone.like.model.LikeEntity;
+import com.likelion.stepstone.post.model.PostEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class LikeService {
+
+    @Autowired
+    private LikeRepository likeRepository;
+
+    public void likes(LikeDto likeDto) {
+        LikeEntity likeEntity = LikeEntity.toEntity(likeDto);
+
+
+        likeRepository.save(likeEntity);
+    }
 }
