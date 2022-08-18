2799ffd26cead805ecaac633386becea9bfbd7b3
nellizi <masterliz9095@gmail.com>
2022-08-18 21:50:04 +0900

    Refactor : Entity 클래스 수정

--- StepStone/src/main/java/com/likelion/stepstone/like/LikeController.java
+++ StepStone/src/main/java/com/likelion/stepstone/like/LikeController.java
@@ -5,7 +5,6 @@ import com.likelion.stepstone.user.model.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
 
 
 @Controller
@@ -15,15 +14,15 @@ public class LikeController {
     private LikeService likeService;
     private UserRepository userRepository;  // userId 받아오기 위한 임시 사용
 
-    @GetMapping("/post/{postId}/likes")
+    @GetMapping("/post/{postCid}/likes")
     @ResponseBody
-    public UUID like(@PathVariable UUID postId) {
+    public Long like(@PathVariable Long postCid) {
 
         UserEntity user = userRepository.findByName("No3").get(); //로그인 정보로 UserId 받아와야 함.(임시)
-         UUID userId = user.getUserId();
+         Long userCid = user.getUserCid();
 
-        likeService.idCheck2(postId, userId);
-        return userId;
+        likeService.idCheck2(postCid, userCid);
+        return userCid;
 
     }
 
