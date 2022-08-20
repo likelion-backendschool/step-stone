3334d2c50c836d9d46ed7918d7506aed3b421b6f
YeonJun Cho <micayel1219@gmail.com>
2022-08-20 22:10:23 +0900

    Feat : 채팅방 Create Post 생성

--- StepStone/src/main/java/com/likelion/stepstone/chatroom/ChatRoomController.java
+++ StepStone/src/main/java/com/likelion/stepstone/chatroom/ChatRoomController.java
@@ -1,7 +1,44 @@
 package com.likelion.stepstone.chatroom;
 
+import com.likelion.stepstone.chatroom.model.ChatRoomForm;
+import com.likelion.stepstone.chatroom.model.ChatRoomDto;
+import com.likelion.stepstone.chatroom.model.ChatRoomVo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Controller
+@RequestMapping("/chat/room")
 public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping("/")
+    public String getRoom(Model model) {
+        return "chat/room";
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public ChatRoomVo createRoom(ChatRoomForm chatRoomForm) {
+
+        ChatRoomDto chatRoomDto = ChatRoomDto.builder()
+                .chatRoomId(UUID.randomUUID())
+                .roomName(chatRoomForm.getRoomName())
+                .postCid(chatRoomForm.getPostCid())
+                .userCount(0)
+                .build();
+
+        return chatRoomService.create(chatRoomDto);
+    }
+    // 채팅방 입장 화면
+    @GetMapping("/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "chat/roomdetail";
+    }
 }
--- StepStone/src/main/java/com/likelion/stepstone/chatroom/ChatRoomService.java
+++ StepStone/src/main/java/com/likelion/stepstone/chatroom/ChatRoomService.java
@@ -1,8 +1,19 @@
 package com.likelion.stepstone.chatroom;
 
+import com.likelion.stepstone.chatroom.model.ChatRoomDto;
+import com.likelion.stepstone.chatroom.model.ChatRoomEntity;
+import com.likelion.stepstone.chatroom.model.ChatRoomVo;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
+
+    public ChatRoomVo create(ChatRoomDto chatRoomDto) {
+        ChatRoomEntity chatRoomEntity = ChatRoomEntity.toEntity(chatRoomDto);
+
+        chatRoomRepository.save(chatRoomEntity);
+
+        return ChatRoomVo.toVo(ChatRoomDto.toDto(chatRoomEntity));
+    }
 }
