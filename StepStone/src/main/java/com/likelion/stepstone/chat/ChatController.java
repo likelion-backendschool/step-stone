e0650d379d105619fe8dc4bf59ae9eb04bc65dfe
YeonJun Cho <micayel1219@gmail.com>
2022-08-23 21:34:33 +0900

    Feat : Chatting Controller, Service 생성

--- StepStone/src/main/java/com/likelion/stepstone/chat/ChatController.java
+++ StepStone/src/main/java/com/likelion/stepstone/chat/ChatController.java
@@ -1,4 +1,28 @@
 package com.likelion.stepstone.chat;
 
+import com.likelion.stepstone.chat.model.ChatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class ChatController {
+
+    private final ChatService chatService;
+    @MessageMapping
+    void sendMessage(ChatDto chatDto){
+        chatDto.setSenderUserCid(1l);
+
+        chatService.sendMessage(chatDto);
+    }
+
+    //Client가 SEND할 수 있는 경로
+    //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
+    //"/pub/chat/enter"
+    @MessageMapping(value = "/chat/enter")
+    public void enter(ChatDto chatDto){
+        chatService.enter(chatDto);
+    }
 }
--- StepStone/src/main/java/com/likelion/stepstone/chat/ChatService.java
+++ StepStone/src/main/java/com/likelion/stepstone/chat/ChatService.java
@@ -1,9 +1,41 @@
 package com.likelion.stepstone.chat;
 
+import com.likelion.stepstone.chat.model.ChatDto;
+import com.likelion.stepstone.chat.model.ChatEntity;
 import com.likelion.stepstone.chatroom.ChatRoomRepository;
-import lombok.AllArgsConstructor;
+import com.likelion.stepstone.chatroom.exception.DataNotFoundException;
+import com.likelion.stepstone.chatroom.model.ChatRoomEntity;
+import com.likelion.stepstone.user.UserRepository;
+import com.likelion.stepstone.user.model.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ChatService {
     private final ChatRepository chatRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final UserRepository userRepository;
+
+    //Client가 SEND할 수 있는 경로
+    //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
+    //"/pub/chat/enter"
+    public void sendMessage(ChatDto chatDto) {
+        ChatEntity chatEntity = ChatEntity.toEntity(chatDto);
+        UserEntity userEntity = userRepository.findById(1l).orElseThrow(()-> new DataNotFoundException("user not found"));
+        chatEntity.setSender(userEntity);
+
+//        if(!chatRoomEntity.getUsers().contains(chatEntity.getSender())){
+//            throw new DataNotFoundException("참가 중인 유저가 아님");
+//        }
+        messagingTemplate.convertAndSend("/sub/chat/room/" + chatDto.getChatRoomId(), ChatDto.toDto(chatEntity));
+    }
+
+    public void enter(ChatDto chatDto) {
+        ChatEntity chatEntity = ChatEntity.toEntity(chatDto);
+        chatEntity.setMessage(chatEntity.getMessage() + "님이 채팅방에 참여하였습니다.");
+
+        messagingTemplate.convertAndSend("/sub/chat/room/" + chatEntity.getChatRoomId(), ChatDto.toDto(chatEntity));
+    }
 }
