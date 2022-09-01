96bb924795d07f39e9eef196e21fcd9ccf6368f3
EunJung803 <aimee3322@gmail.com>
2022-09-01 18:23:46 +0900

    Fix/Feat : layout과 UI 이상현상 수정 / 최근 이슈 게시글 - 게시글 갯수에 따른 case 분리 및 타임리프 수정  layout에 card_carousel_style css가 모두 적용되는거 수정, 게시글 갯수에 따른 view 수정 (likes 0개는 최근 이슈 게시글에 띄우지 않기 부분 구현 예정)

--- StepStone/src/main/java/com/likelion/stepstone/home/HomeController.java
+++ StepStone/src/main/java/com/likelion/stepstone/home/HomeController.java
@@ -26,13 +26,56 @@ public class HomeController {
     public String issuedPost(Model model) {
         List<PostVo> postVoList = postService.getSortedPostList();
 
-        List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
-        List<PostVo> postVoList2 = new ArrayList<>(postVoList.subList(3, 6));
-        List<PostVo> postVoList3 = new ArrayList<>(postVoList.subList(6, 9));
+        int postVoListSize = postVoList.size();
 
-        model.addAttribute("posts1", postVoList1);
-        model.addAttribute("posts2", postVoList2);
-        model.addAttribute("posts3", postVoList3);
+        // 게시글이 9개 이상 -> like 정렬 -> 최근 이슈 게시글에는 top 9까지 띄우기 (slide 1, 2 사용)
+        if (postVoListSize >= 9) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+            List<PostVo> postVoList2 = new ArrayList<>(postVoList.subList(3, 6));
+            List<PostVo> postVoList3 = new ArrayList<>(postVoList.subList(6, 9));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+            model.addAttribute("posts2", postVoList2);
+            model.addAttribute("posts3", postVoList3);
+
+            return "index";
+        }
+
+        // 게시글이 6개 ~ 8개 -> like 정렬 -> 최근 이슈 게시글에는 top 6까지 띄우기 (slide 1, 2 사용)
+        if (postVoListSize >= 6) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+            List<PostVo> postVoList2 = new ArrayList<>(postVoList.subList(3, 6));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+            model.addAttribute("posts2", postVoList2);
+
+            return "index";
+        }
+
+        // 게시글이 3개 ~ 5개 -> like 정렬 -> 최근 이슈 게시글에는 그 중 top 3만 띄우기 (slide 1만 사용)
+        if (postVoListSize > 2 && postVoListSize < 6) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+
+            return "index";
+        }
+
+        // 게시글이 1개 or 2개 -> like 정렬 -> 최근 이슈 게시글에 우선 모두 띄우기 (slide 1만 사용)
+        if (postVoListSize > 0) {
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList);
+
+            return "index";
+        }
+
+        // 게시글이 존재하지 않는 경우 (0개 이하)
+        else {
+            model.addAttribute("postSize", postVoList.size());
+        }
 
         return "index";
     }
