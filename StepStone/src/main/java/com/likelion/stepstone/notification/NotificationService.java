b561539ea94568e39361b21f3929fb155cf96a21
YeonJun Cho <micayel1219@gmail.com>
2022-09-14 16:03:48 +0900

    Feat : 알림 Controller, Service 생성

---
+++ StepStone/src/main/java/com/likelion/stepstone/notification/NotificationService.java
@@ -0,0 +1,38 @@
+package com.likelion.stepstone.notification;
+
+
+import com.likelion.stepstone.chatroom.exception.DataNotFoundException;
+import com.likelion.stepstone.notification.model.NotificationDto;
+import com.likelion.stepstone.notification.model.NotificationEntity;
+import com.likelion.stepstone.user.UserRepository;
+import com.likelion.stepstone.user.model.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    private final UserRepository userRepository;
+
+    public void markAsRead(List<NotificationEntity> notifications) {
+        notifications.forEach(NotificationEntity::read);
+    }
+
+    public List<NotificationDto> readNewNotifications(String userId){
+        UserEntity userEntity = userRepository.findByUserId(userId).orElseThrow(() -> new DataNotFoundException("user not found"));
+        List<NotificationEntity> notificationEntities = notificationRepository.findByUserEntityAndChecked(userEntity, false);
+        markAsRead(notificationEntities);
+
+        List<NotificationDto> notificationDtos = notificationEntities.stream().map(NotificationDto::toDto).toList();
+
+        return notificationDtos;
+    }
+
+}
