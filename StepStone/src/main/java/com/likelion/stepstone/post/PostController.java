dfdad59f5da4796b98251907c0aec7c23fcc9e06
EunJung803 <aimee3322@gmail.com>
2022-09-15 03:54:33 +0900

    Feat : 게시글 목록에 최근 이슈 게시글 부분 추가 card css 전체 적용으로 인해 id로 따로 선택적 css 적용

--- StepStone/src/main/java/com/likelion/stepstone/post/PostController.java
+++ StepStone/src/main/java/com/likelion/stepstone/post/PostController.java
@@ -5,6 +5,7 @@ import com.likelion.stepstone.like.model.LikeDto;
 import com.likelion.stepstone.like.model.LikeEntity;
 import com.likelion.stepstone.post.model.PostDto;
 import com.likelion.stepstone.post.model.PostEntity;
+import com.likelion.stepstone.post.model.PostVo;
 import com.likelion.stepstone.user.UserService;
 import com.likelion.stepstone.user.model.UserDto;
 import com.likelion.stepstone.user.model.UserEntity;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -40,7 +42,6 @@ public class PostController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-
     public String create(Principal principal,Model model, PostForm postForm) {
 
         //유효성 체크
@@ -74,11 +75,66 @@ public class PostController {
     }
 
     @GetMapping("/list")
-    public String list(Principal principal,Model model, @RequestParam(defaultValue = "0") int page ) {
+    public String list(Principal principal, Model model, @RequestParam(defaultValue = "0") int page ) {
 
         UserDto user = userService.getUser(principal.getName());  //현재 로그인 한 user
+
         Page<PostEntity> paging = postService.getList(page , user);
         model.addAttribute("paging", paging);
+
+        // 최근 이슈 게시글 부분
+        List<PostVo> postVoList = postService.getSortedPostList();
+        int postVoListSize = postVoList.size();
+
+        // 게시글이 9개 이상 -> like 정렬 -> 최근 이슈 게시글에는 top 9까지 띄우기 (slide 1, 2 사용)
+        if (postVoListSize >= 9) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+            List<PostVo> postVoList2 = new ArrayList<>(postVoList.subList(3, 6));
+            List<PostVo> postVoList3 = new ArrayList<>(postVoList.subList(6, 9));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+            model.addAttribute("posts2", postVoList2);
+            model.addAttribute("posts3", postVoList3);
+
+            return "post/list";
+        }
+
+        // 게시글이 6개 ~ 8개 -> like 정렬 -> 최근 이슈 게시글에는 top 6까지 띄우기 (slide 1, 2 사용)
+        if (postVoListSize >= 6) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+            List<PostVo> postVoList2 = new ArrayList<>(postVoList.subList(3, 6));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+            model.addAttribute("posts2", postVoList2);
+
+            return "post/list";
+        }
+
+        // 게시글이 3개 ~ 5개 -> like 정렬 -> 최근 이슈 게시글에는 그 중 top 3만 띄우기 (slide 1만 사용)
+        if (postVoListSize > 2 && postVoListSize < 6) {
+            List<PostVo> postVoList1 = new ArrayList<>(postVoList.subList(0, 3));
+
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList1);
+
+            return "post/list";
+        }
+
+        // 게시글이 1개 or 2개 -> like 정렬 -> 최근 이슈 게시글에 우선 모두 띄우기 (slide 1만 사용)
+        if (postVoListSize > 0) {
+            model.addAttribute("postSize", postVoList.size());
+            model.addAttribute("posts1", postVoList);
+
+            return "post/list";
+        }
+
+        // 게시글이 존재하지 않는 경우 (0개 이하)
+        else {
+            model.addAttribute("postSize", postVoList.size());
+        }
+
         return "post/list";
     }
 
