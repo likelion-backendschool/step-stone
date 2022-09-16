2f1dc365c59fe04c6deb56a6678eda0e2d9ebd1c
YeonJun Cho <micayel1219@gmail.com>
2022-09-17 01:21:42 +0900

    Feat : 새로운 채팅 발생 이벤트, 리스너 생성

---
+++ StepStone/src/main/java/com/likelion/stepstone/chat/event/ChatSendEvent.java
@@ -0,0 +1,18 @@
+package com.likelion.stepstone.chat.event;
+
+import com.likelion.stepstone.chat.model.ChatDto;
+import com.likelion.stepstone.chat.model.ChatEntity;
+import com.likelion.stepstone.user.model.UserEntity;
+import lombok.Getter;
+
+@Getter
+public class ChatSendEvent {
+    private final ChatEntity chatEntity;
+    private final UserEntity userEntity;
+
+    public ChatSendEvent(ChatEntity chatEntity, UserEntity userEntity) {
+        this.chatEntity = chatEntity;
+        this.userEntity = userEntity;
+
+    }
+}
--- StepStone/src/main/java/com/likelion/stepstone/notification/model/NotificationType.java
+++ StepStone/src/main/java/com/likelion/stepstone/notification/model/NotificationType.java
@@ -3,6 +3,7 @@ package com.likelion.stepstone.notification.model;
 public enum NotificationType {
     NEW_MESSAGE,
     CHAT_REQUEST,
+    CHAT_SEND,
     PROJECT_START,
     CHAT_ROOM_CREATED
 }
