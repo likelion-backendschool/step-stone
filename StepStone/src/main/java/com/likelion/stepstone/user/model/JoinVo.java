6b66400f5b9cf761304751669b14c7433bf69007
jwp345 <jwp135@naver.com>
2022-09-17 23:05:43 +0900

    Refactor : 회원가입 패스워드 길이 제한

--- StepStone/src/main/java/com/likelion/stepstone/user/model/JoinVo.java
+++ StepStone/src/main/java/com/likelion/stepstone/user/model/JoinVo.java
@@ -3,7 +3,6 @@ package com.likelion.stepstone.user.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -12,7 +11,7 @@ import javax.validation.constraints.Size;
 @Getter
 @RequiredArgsConstructor
 public class JoinVo {
-  @Size(min = 2, max = 25, message = "성과 이름을 입력해 주세요.")
+  @Size(min = 2, max = 25, message = "이름은 2글자 이상 25자 미만 입력해 주세요.")
   @NotEmpty(message = "이름은 필수항목 입니다.")
   private final String name;
 
@@ -20,6 +19,7 @@ public class JoinVo {
   @NotEmpty(message = "사용자ID는 필수항목입니다.")
   private final String userId;
 
+  @Size(min = 8, max = 25, message = "비밀번호는 8글자 이상 25글자 미만으로 입력해 주세요.")
   @NotEmpty(message = "비밀번호는 필수항목입니다.")
   private final String password;
 
