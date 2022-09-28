3393deecf13380b7010e94577b4edf163839af89
jwp345 <jwp135@naver.com>
2022-09-29 00:49:48 +0900

    Fix : CORSFilter Error 수정

--- StepStone/src/main/java/com/likelion/stepstone/config/CorsConfig.java
+++ StepStone/src/main/java/com/likelion/stepstone/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
-    config.addAllowedOrigin("*"); // e.g. http://domain1.com(모든 ip에 응답을 허용하겠다)
+//    config.addAllowedOrigin("*"); // e.g. http://domain1.com(모든 ip에 응답을 허용하겠다)
     config.addAllowedHeader("*"); // 모든 header에 응답을 허용
     config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청을 허용
+    config.addAllowedOriginPattern("*");
 
     source.registerCorsConfiguration("/**", config);
 
